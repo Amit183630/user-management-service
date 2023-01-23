c1859ca75f018737be82fb4531898c68851bb8e9
Amit183630 <59386584+Amit183630@users.noreply.github.com>
2023-01-23 06:34:53 +0000

    test endpoint added

--- src/main/java/com/ongraph/usermanagementapp/controller/UserController.java
+++ src/main/java/com/ongraph/usermanagementapp/controller/UserController.java
@@ -1,5 +1,14 @@
 package com.ongraph.usermanagementapp.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
 public class UserController {
 
+    @GetMapping("/")
+    public String getMethodName() {
+        return "Testing.....";
+    }
+    
 }
